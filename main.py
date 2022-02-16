01cd9f3c07d994dd73de21c408329a9348128d78
Ruslan <ruslanatsabryk@gmail.com>
2022-02-16 10:10:52 +0300

    Core voice recognition code

--- main.py
+++ main.py
@@ -1,5 +1,54 @@
+import speech_recognition as sr
+import pyttsx3
+from tasks import *
+from pprint import pprint
 
 
 
 if __name__ == "__main__":
-    pass
+
+    tasks = {
+        "start_browser": ["start browser", "open browser", "internet"],
+        "start_vs_code": ["vs code", "editor"],
+        "shutdown_computer": ["computer shutdown", "power off"]
+    }
+    
+    # Listening parameters
+    listener = sr.Recognizer()
+    listener.energy_threshold = 600  # 300 minimum audio energy to consider for recording
+    listener.dynamic_energy_threshold = True  # True
+    listener.dynamic_energy_adjustment_damping = 0.15 # 0.15
+    listener.dynamic_energy_ratio = 1.5 # 1.5
+    listener.pause_threshold = 0.6  # seconds of non-speaking audio before a phrase is considered complete
+    listener.operation_timeout = None  # seconds after an internal operation (e.g., an API request) starts before it times out, or ``None`` for no timeout
+    listener.phrase_threshold = 0.3  # 0.3 minimum seconds of speaking audio before we consider the speaking audio a phrase - values below this are ignored (for filtering out clicks and pops)
+    listener.non_speaking_duration = 0.5  # 0.5 seconds of non-speaking audio to keep on both sides of the recording
+
+    talker = pyttsx3.init()
+    talker.setProperty("rate", 150)
+    voices = talker.getProperty("voices")
+    talker.setProperty("voice", voices[1].id)
+    
+
+    talker.say("Listen to you carifully my dear user!")
+    talker.runAndWait()
+
+    # Listening loop
+    while True:
+        try:
+            with sr.Microphone() as source:
+                listener.adjust_for_ambient_noise(source=source, duration=0.2)
+                phrase_voice = listener.listen(source)
+                phrase_text = listener.recognize_google(phrase_voice, language="en-US")
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+        print(phrase_text)
+        
+        # Execute function by key from global dictionary
+        for fun, v in tasks.items():
+            if phrase_text in v:
+                globals()[fun]()
+        phrase_text = ''
---
+++ tasks.py
@@ -0,0 +1,8 @@
+def start_browser():
+    print("Browser started")
+
+def start_vs_code():
+    print("VS Code started")
+
+def shutdown_computer():
+    print("Computer shutting down")
